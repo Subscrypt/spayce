37531e6e2396d9c2c83cbf0780b30bef963cf691
Andrey Ezhov <rybakisa@gmail.com>
2023-08-09 15:19:32 +0400

    Init Subscrypt v2 (#6)
    Init new cleaner version of the application without unnecessary code brought from Scaffold ETH 2
    
    ---------
    Co-authored-by: Ivan <grang2kk@gmail.com>

---
+++ src/app/components/subscription/Options.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from "react";
+import Button from "../Button";
+
+interface Props {
+  children?: ReactNode;
+  onBlur: () => void;
+}
+
+const Options = ({ children, onBlur }: Props) => {
+  return (
+    <div
+      className="flex flex-col gap-1 p-1 rounded-2xl bg-white stroke-gray-100 drop-shadow-xl"
+      onBlur={onBlur}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Options;

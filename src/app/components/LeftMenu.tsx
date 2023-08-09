17c2d29eee3e6f8d6cae8a5b15e0be73f90b9563
Andrey Ezhov <rybakisa@gmail.com>
2023-08-09 15:12:38 +0400

    Fix build

--- src/app/components/LeftMenu.tsx
+++ src/app/components/LeftMenu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, from "react";
+import React from "react";
 import Button from "./MenuButton";
 import { usePathname, useRouter } from "next/navigation";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";

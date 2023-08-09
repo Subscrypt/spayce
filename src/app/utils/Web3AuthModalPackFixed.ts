fe1a408f85b816a72c6e66143f64b31d7fc00b3b
Andrey Ezhov <rybakisa@gmail.com>
2023-08-09 20:26:08 +0400

    Fix typing discrepancy for auth adapters

--- src/app/utils/Web3AuthModalPackFixed.ts
+++ src/app/utils/Web3AuthModalPackFixed.ts
@@ -1,16 +1,23 @@
 import { Web3AuthModalPack } from '@safe-global/auth-kit'
 import { IAdapter } from '@web3auth/base';
 import { ModalConfig, Web3AuthOptions } from '@web3auth/modal';
-import { AdapterNamespaceType } from "@web3auth/base";
+
+export declare const ADAPTER_NAMESPACES: {
+    readonly EIP155: "eip155";
+    readonly SOLANA: "solana";
+    readonly MULTICHAIN: "multichain";
+    // readonly XRPL: "xrpl";
+};
+export type AdapterNamespaceTypeFixed = (typeof ADAPTER_NAMESPACES)[keyof typeof ADAPTER_NAMESPACES];
 
 export declare interface IAdapterFixed<T> extends IAdapter<T> {
-    adapterNamespace: AdapterNamespaceType;
+    adapterNamespace: AdapterNamespaceTypeFixed;
 }
 
 export declare class Web3AuthModalPackFixed extends Web3AuthModalPack {
-    override init({ options, adapters, modalConfig }: {
+    init({ options, adapters, modalConfig }: {
         options: Web3AuthOptions;
-        adapters?: IAdapter<unknown>[];
+        adapters?: IAdapterFixed<unknown>[];
         modalConfig?: Record<string, ModalConfig>;
     }): Promise<void>;
 }

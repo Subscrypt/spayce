8c1cc94fd25d78bc17839019fc4f904e0d89ad97
Ivan <grang2kk@gmail.com>
2023-08-11 12:11:36 +0400

    Fix route

--- src/app/api/users/custom/route.ts
+++ src/app/api/users/custom/route.ts
@@ -2,5 +2,5 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
 
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  return NextResponse.json([])
+  return NextResponse.json('Hello World!')
 }

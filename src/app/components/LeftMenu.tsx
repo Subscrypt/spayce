2357eb2406e0befb74416c701e5552cffe33a9be
Ivan <grang2kk@gmail.com>
2023-08-13 01:19:30 +0400

    Add menu for landing

--- src/app/components/LeftMenu.tsx
+++ src/app/components/LeftMenu.tsx
@@ -4,6 +4,7 @@ import Button from "./MenuButton";
 import { usePathname, useRouter } from "next/navigation";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 import CreateSpayceButtom from "./CreateSpayceButton"
+import { useAccountAbstraction } from "../store/safe/accountAbstractionContext";
 
 const LeftMenu = () => {
   const handleNavigation = (path: string, router: AppRouterInstance) => {
@@ -12,45 +13,76 @@ const LeftMenu = () => {
 
   const router = useRouter();
   const pathName = usePathname;
+  const { isAuthenticated } = useAccountAbstraction()
 
   return (
     <div className="rounded-2xl justify-between md:w-64 sm:static absolute sm:w-fit bottom-2 border-gray-50 sm:border-0 border-2 w-[98%] sm:left-0 left-[1%] z-10 bg-white flex sm:flex-col flex-row gap-1">
-      <div className="flex flex-row sm:flex-col gap-1 p-2">
-        <CreateSpayceButtom onClick={() => router.push('/createSpayce')} />
-        <Button
-          icon="🖊️"
-          text="Subscriptions"
-          onClick={() => handleNavigation("/", router)}
-          isFocused={pathName() === "/" ? true : false}
-        />
 
-        <Button
-          icon="👔"
-          text="Marketplace"
-          onClick={() => handleNavigation("/marketplace", router)}
-          isFocused={pathName() === "/marketplace" ? true : false}
-        />
-        <Button
-          icon="📃"
-          text="Billings"
-          onClick={() => handleNavigation("/billings", router)}
-          isFocused={pathName() === "/billings" ? true : false}
-        />
-      </div>
-      <div className="flex flex-row sm:flex-col gap-1 p-2">
-        <Button
-          icon="⚙️"
-          text="Dev"
-          onClick={() => handleNavigation("/docs/nav", router)}
-          isFocused={pathName() === "/docs/nav" ? true : false}
-        />
-        <Button
-          icon="📃"
-          text="Swagger"
-          onClick={() => handleNavigation("/docs", router)}
-          isFocused={pathName() === "/docs" ? true : false}
-        />
-      </div>
+      {isAuthenticated ?
+        <>
+          <div className="flex flex-row sm:flex-col gap-1 p-2">
+            <CreateSpayceButtom onClick={() => router.push('/createSpayce')} />
+            <Button
+              icon="🖊️"
+              text="Subscriptions"
+              onClick={() => handleNavigation("/", router)}
+              isFocused={pathName() === "/" ? true : false}
+            />
+
+            <Button
+              icon="👔"
+              text="Marketplace"
+              onClick={() => handleNavigation("/marketplace", router)}
+              isFocused={pathName() === "/marketplace" ? true : false}
+            />
+            <Button
+              icon="📃"
+              text="Billings"
+              onClick={() => handleNavigation("/billings", router)}
+              isFocused={pathName() === "/billings" ? true : false}
+            />
+          </div>
+          <div className="flex flex-row sm:flex-col gap-1 p-2">
+            <Button
+              icon="⚙️"
+              text="Dev"
+              onClick={() => handleNavigation("/docs/nav", router)}
+              isFocused={pathName() === "/docs/nav" ? true : false}
+            />
+            <Button
+              icon="📃"
+              text="Swagger"
+              onClick={() => handleNavigation("/docs", router)}
+              isFocused={pathName() === "/docs" ? true : false}
+            />
+          </div>
+        </>
+        :
+        <>
+          <div className="flex flex-row sm:flex-col gap-1 p-2">
+            <Button
+              icon="🚀"
+              text="About"
+              onClick={() => handleNavigation("/#about", router)}
+              isFocused={pathName() === "/#about" ? true : false}
+            />
+            <Button
+              icon="🛠️"
+              text="How it works"
+              onClick={() => handleNavigation("/#howitworks", router)}
+              isFocused={pathName() === "/#howitworks" ? true : false}
+            />
+            <Button
+              icon="📝"
+              text="Features"
+              onClick={() => handleNavigation("/#features", router)}
+              isFocused={pathName() === "/#features" ? true : false}
+            />
+          </div>
+        </>
+      }
+
+
     </div>
   );
 };
--- src/app/components/landing/Landing.tsx
+++ src/app/components/landing/Landing.tsx
@@ -6,9 +6,10 @@ import { useAccountAbstraction } from "../../store/safe/accountAbstractionContex
 
 const Landing = () => {
     const { loginWeb3Auth } = useAccountAbstraction()
+
     return (
         <div className='overflow-scroll rounded-2xl'>
-            <div>
+            <div id='about'>
                 <div className='border-2 border-gray-100 relative overflow-hidden p-12 flex flex-col justify-between items-start w-full h-[800px] bg-gray-50 rounded-2xl'>
                     <div className='flex flex-row gap-2 font-semibold uppercase items-center text-gray-600'>made proudly using <img className='w-6 h-6' src='/img/landing/safe.png' /> and <img className='w-6 h-6' src='/img/landing/base.png' /> during Superhack</div>
                     <div>
@@ -23,7 +24,7 @@ const Landing = () => {
                         <button className="py-4 px-6 rounded-xl font-bold transition-colors bg-green-400 hover:bg-green-500 active:bg-green-600 mt-8" onClick={loginWeb3Auth}>Log In to Run App</button>
                     </div>
                 </div>
-                <div className='flex flex-col gap-6 px-12 py-20 w-2/3 text-5xl font-bold text-gray-900 leading-none'>
+                <div id='howitworks' className='flex flex-col gap-6 px-12 py-20 w-2/3 text-5xl font-bold text-gray-900 leading-none'>
                     <span>Everybody loves <span className='text-red-500'>Netflix</span></span>
                     <div className='w-full text-2xl font-medium text-gray-900 leading-normal'>But with its price, it just doesn&apos;t feel like a good value. So you plan to share a Premium Plan for $12.99 for 5 people, but your friend Jake is not a reliable person to pay his share, and you don&apos;t have the rest 3 to share your lovely streaming service.<br /> And also, <span className='font-bold'>you don&apos;t want to pay with your card all by yourself</span></div>
                 </div>
@@ -49,7 +50,7 @@ const Landing = () => {
                         </div>
                     </div>
                 </div>
-                <div className='flex flex-col gap-6 px-12 py-20 w-2/3 text-5xl font-bold text-gray-900 leading-none'>
+                <div id='features' className='flex flex-col gap-6 px-12 py-20 w-2/3 text-5xl font-bold text-gray-900 leading-none'>
                     <span>Multisignature Wallets</span>
                     <div className='w-full text-2xl font-medium text-gray-900 leading-normal'>But with its price</div>
                 </div>

7631bd74273d556ed82ecac711bac6e9fa4b07db
Andrey Ezhov <rybakisa@gmail.com>
2023-08-13 22:06:15 +0400

    Fix build

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/(.*)",
+                source: "/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },

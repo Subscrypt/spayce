39e125503ff1232898fd04fea67e71caf1116a56
Andrey Ezhov <rybakisa@gmail.com>
2023-08-13 20:00:09 +0400

    Add Safe SDK Stack (#11)
    * Add Safe SDK samples
    * Convert exmaple to module
    * Add Safe SDK snippets
    * Change network to Base Goerli
    * Add signing with external wallet snippet
    * Add Safe Wallet creation flow
    Add logic to deploy a Safe Wallet when last member applying to subscription
    * Fix typing for snippets
    * Fix typing errors
    * Add suscription shares smart contract

---
+++ src/app/api/safe/create_wallet/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import { deploySafe } from "../../../utils/safe/protocol-kit/deploy-safe";
+
+
+export async function GET() {
+  const config = {
+    RPC_URL: process.env.BASE_RPC_URL || "",
+    DEPLOYER_ADDRESS_PRIVATE_KEY: process.env.DEPLOYER_ADDRESS_PRIVATE_KEY || "",
+    DEPLOY_SAFE: {
+      OWNERS: [process.env.DEPLOYER_ADDRESS || "", process.env.OWNER_1_ADDRESS || "", process.env.OWNER_2_ADDRESS || ""],
+      THRESHOLD: 3,
+      SALT_NONCE: '0'
+    }
+  }
+  
+  const safeAddress = await deploySafe(config)
+  return NextResponse.json({ safeAddress })
+}
